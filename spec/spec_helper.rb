50019774fc4a464fde39366a8d679d8ee64e73c3
Robert Wahler <robert@gearheadforhire.com>
2009-03-19 13:43:24 -0400

    moved spec requires to helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,21 +1,6 @@
-#require File.dirname(__FILE__) + '/../lib/textile_template'
- 
-def turn_methods_public(klass, method_name = nil)
-  if method_name
-    klass.class_eval do
-      public method_name
-    end
-  else
-    turn_all_methods_public klass
-  end
-end
- 
-def turn_all_methods_public(klass)
-  klass.class_eval do
-    private_instance_methods.each { |instance_method| public instance_method }
-    private_methods.each { |method| public_class_method method }
-    protected_instance_methods.each { |instance_method| public instance_method }
-    protected_methods.each { |method| public_class_method method }
-  end
-end
- 
+require 'action_controller'
+require 'action_controller/cgi_ext'
+require 'action_controller/test_process'
+require 'action_view/test_case'
+require 'RedCloth'
+require File.dirname(__FILE__) + '/../lib/textile_template'
--- spec/textile_template/textile_template_spec.rb
+++ spec/textile_template/textile_template_spec.rb
@@ -1,10 +1,4 @@
 require File.dirname(__FILE__) + '/../spec_helper'
-require 'action_controller'
-require 'action_controller/cgi_ext'
-require 'action_controller/test_process'
-require 'action_view/test_case'
-require 'RedCloth'
-require File.dirname(__FILE__) + '/../../lib/textile_template'
  
 describe ActionView::TemplateHandlers::TextileTemplate do
   
@@ -18,24 +12,18 @@ describe ActionView::TemplateHandlers::TextileTemplate do
   def render(*args)
     ActionController::Base.view_paths = PATH
     result = ActionView::Base.new(ActionController::Base.view_paths , {}).render(*args)
-    # add the trailing newline that all our fixtures have
-    #result + "\n"
   end
  
   describe "initialize" do
-    
     it "should store view" do
       @template.instance_variable_get(:@view).should == @view_mock
     end
- 
   end
   
   describe "render" do
-    
     it "should return html generated from textile" do
       render(:file => 'test.textile').should == File.read(PATH + "/test.html").strip
     end
- 
   end
   
 end
