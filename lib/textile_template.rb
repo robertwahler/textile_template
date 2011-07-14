3179d900091a4eb785fa41dfa026c642b078aba2
Robert Wahler <robert@gearheadforhire.com>
2011-07-14 14:44:35 -0400

    html_safe for rails 2.3.8+

--- lib/textile_template.rb
+++ lib/textile_template.rb
@@ -3,23 +3,23 @@ module ActionView
     class TextileTemplate < ERB
       def compile(template)
         src = super
-        "_textile_proc = proc {\n#{src}\n}; RedCloth.new(_textile_proc.call, [:hard_breaks]).to_html"
+        "_textile_proc = proc {\n#{src}\n}; RedCloth.new(_textile_proc.call, [:hard_breaks]).to_html.html_safe"
       end
- 
+
       def cache_fragment(block, name = {}, options = nil) #:nodoc:
         @view.fragment_for(block, name, options) do
           render_textile(eval(ActionView::Base.erb_variable, block.binding))
         end
       end
- 
+
       private
       def render_textile(result)
-        RedCloth.new(result, [:hard_breaks]).to_html
+        RedCloth.new(result, [:hard_breaks]).to_html.html_safe
       end
     end
   end
 
-  Template.register_template_handler :textile, TemplateHandlers::TextileTemplate 
+  Template.register_template_handler :textile, TemplateHandlers::TextileTemplate
 
   module VERSION #nodoc
     MAJOR = 0
